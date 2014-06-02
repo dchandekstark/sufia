a3f4dcd06e1e2f047191ca004a0408a14388e223
dchandekstark <dchandekstark@gmail.com>
2014-06-02 16:55:18 -0400

    Refactored anonymous event handlers into named functions. Fixes #488

--- app/assets/javascripts/sufia/permissions.js
+++ app/assets/javascripts/sufia/permissions.js
@@ -9,16 +9,46 @@
    */
 
 Blacklight.onLoad(function() {
-  // input for uids -  attach function to verify uid
-  $('#new_user_name_skel').on('blur', function() {
-      // clear out any existing messages
-      $('#directory_user_result').html('');
-      var un = $('#new_user_name_skel').val();
-      var perm = $('#new_user_permission_skel').val();
-      if ( $.trim(un).length == 0 ) {
+	// input for uids -  attach function to verify uid
+	$('#new_user_name_skel').on('blur', verify_user_for_permission);
+
+	// add button for new user
+	$('#add_new_user_skel').on('click', add_user_permission);
+
+	// add button for new group
+	$('#add_new_group_skel').on('click', add_group_permission);
+
+	// when user clicks on visibility, update potential access levels
+	$("input[name='visibility']").on("change", set_access_levels);
+
+	$('#generic_file_permissions_new_group_name').change(function (){
+		var edit_option = $("#generic_file_permissions_new_group_permission option[value='edit']")[0];
+	    if (this.value.toUpperCase() == 'PUBLIC') {
+	       edit_option.disabled =true;
+	    } else {
+           edit_option.disabled =false;
+	    }
+
+	});
+
+	$('.remove_perm').on('click', function() {
+		var top = $(this).parent().parent();
+		top.hide(); // do not show the block
+		top.find('.select_perm')[0].options[0].selected= true; // select the first otion which is none
+		return false;
+	});
+});
+
+// Verify that user exists in the directory
+function verify_user_for_permission {
+    // clear out any existing messages
+    $('#directory_user_result').html('');
+    var un = $('#new_user_name_skel').val();
+    var perm = $('#new_user_permission_skel').val();
+    if ( $.trim(un).length == 0 ) {
         return;
-      }
-      $.ajax( {
+    }
+    $.ajax( {
         url: "/directory/user/" + un,
         success: function( data ) {
           if (data != null) {
@@ -33,123 +63,99 @@ Blacklight.onLoad(function() {
             }
           }
         }
-      });
+    });
+} // lookup_user
 
-  });
+// Add a permission for a user or group
+function addPerm(un, perm_form, perm, perm_type) {
+    var tr = $(document.createElement('tr'));
+    var td1 = $(document.createElement('td'));
+    var td2 = $(document.createElement('td'));
+    var remove = $('<button class="btn close">X</button>');
+	
+    $('#save_perm_note').show();
 
-  // add button for new user
-  $('#add_new_user_skel').on('click', function() {
-      if ($('#new_user_name_skel').val() == "" || $('#new_user_permission_skel :selected').index() == "0") {
+    $('#new_perms').append(td1);
+    $('#new_perms').append(td2);
+
+    td1.html('<label class="control-label">'+un+'</label>');
+    td2.html(perm);
+    td2.append(remove);
+    remove.click(function () {
+        tr.remove();
+    });
+
+    $('<input>').attr({
+        type: 'hidden',
+        name: 'generic_file[permissions]['+perm_type+']['+un+']',
+        value: perm_form
+    }).appendTo(td2);
+    tr.append(td1);
+    tr.append(td2);
+    $('#file_permissions').after(tr);
+    tr.effect("highlight", {}, 3000);
+}
+
+// Add a new user permission
+function add_user_permission {
+    if ($('#new_user_name_skel').val() == "" || $('#new_user_permission_skel :selected').index() == "0") {
         $('#new_user_name_skel').focus();
         return false;
-      }
+    }
 
-      if ($('#new_user_name_skel').val() == $('#file_owner').html()) {
+    if ($('#new_user_name_skel').val() == $('#file_owner').html()) {
         $('#permissions_error_text').html("Cannot change owner permissions.");
         $('#permissions_error').show();
         $('#new_user_name_skel').val('');
         $('#new_user_name_skel').focus();
         return false;
-      }
+    }
 
-      if (!is_permission_duplicate($('#new_user_name_skel').val())) {
+    if (!is_permission_duplicate($('#new_user_name_skel').val())) {
         $('#permissions_error_text').html("This user already has a permission.");
         $('#permissions_error').show();
         $('#new_user_name_skel').focus();
         return false;
-      }
-      $('#permissions_error').html();
-      $('#permissions_error').hide();
+    }
+    $('#permissions_error').html();
+    $('#permissions_error').hide();
 
-      var un = $('#new_user_name_skel').val();
-      var perm_form = $('#new_user_permission_skel').val();
-      var perm = $('#new_user_permission_skel :selected').text();
-      // clear out the elements to add more
-      $('#new_user_name_skel').val('');
-      $('#new_user_permission_skel').val('none');
+    var un = $('#new_user_name_skel').val();
+    var perm_form = $('#new_user_permission_skel').val();
+    var perm = $('#new_user_permission_skel :selected').text();
+    // clear out the elements to add more
+    $('#new_user_name_skel').val('');
+    $('#new_user_permission_skel').val('none');
 
-      addPerm(un, perm_form, perm, 'new_user_name');
-      return false;
-  });
+    addPerm(un, perm_form, perm, 'new_user_name');
+    return false;
+}
 
-  // add button for new user
-  $('#add_new_group_skel').on('click', function() {
-      if ($('#new_group_name_skel :selected').index() == "0" || $('#new_group_permission_skel :selected').index() == "0") {
+// Add a new group permission
+function add_group_permission {
+    if ($('#new_group_name_skel :selected').index() == "0" || $('#new_group_permission_skel :selected').index() == "0") {
         $('#new_group_name_skel').focus();
         return false;
-      }
-      var cn = $('#new_group_name_skel').val();
-      var perm_form = $('#new_group_permission_skel').val();
-      var perm = $('#new_group_permission_skel :selected').text();
+    }
+    var cn = $('#new_group_name_skel').val();
+    var perm_form = $('#new_group_permission_skel').val();
+    var perm = $('#new_group_permission_skel :selected').text();
 
-      if (!is_permission_duplicate($('#new_group_name_skel').val())) {
+    if (!is_permission_duplicate($('#new_group_name_skel').val())) {
         $('#permissions_error_text').html("This group already has a permission.");
         $('#permissions_error').show();
         $('#new_group_name_skel').focus();
         return false;
-      }
-      $('#permissions_error').html();
-      $('#permissions_error').hide();
-      // clear out the elements to add more
-      $('#new_group_name_skel').val('');
-      $('#new_group_permission_skel').val('none');
+    }
+    $('#permissions_error').html();
+    $('#permissions_error').hide();
+    // clear out the elements to add more
+    $('#new_group_name_skel').val('');
+    $('#new_group_permission_skel').val('none');
 
-      addPerm(cn, perm_form, perm, 'new_group_name');
-      return false;
-  });
-
-  // when user clicks on visibility, update potential access levels
-  $("input[name='visibility']").on("change", set_access_levels);
-
-	$('#generic_file_permissions_new_group_name').change(function (){
-      var edit_option = $("#generic_file_permissions_new_group_permission option[value='edit']")[0];
-	    if (this.value.toUpperCase() == 'PUBLIC') {
-	       edit_option.disabled =true;
-	    } else {
-           edit_option.disabled =false;
-	    }
-
-	});
-
-  function addPerm(un, perm_form, perm, perm_type)
-  {
-      var tr = $(document.createElement('tr'));
-      var td1 = $(document.createElement('td'));
-      var td2 = $(document.createElement('td'));
-      var remove = $('<button class="btn close">X</button>');
-
-      $('#save_perm_note').show();
-
-      $('#new_perms').append(td1);
-      $('#new_perms').append(td2);
-
-      td1.html('<label class="control-label">'+un+'</label>');
-      td2.html(perm);
-      td2.append(remove);
-      remove.click(function () {
-        tr.remove();
-      });
-
-      $('<input>').attr({
-          type: 'hidden',
-          name: 'generic_file[permissions]['+perm_type+']['+un+']',
-          value: perm_form
-        }).appendTo(td2);
-      tr.append(td1);
-      tr.append(td2);
-      $('#file_permissions').after(tr);
-      tr.effect("highlight", {}, 3000);
-  }
-
-  $('.remove_perm').on('click', function() {
-     var top = $(this).parent().parent();
-     top.hide(); // do not show the block
-     top.find('.select_perm')[0].options[0].selected= true; // select the first otion which is none
-     return false;
-
-  });
-
-});
+    addPerm(cn, perm_form, perm, 'new_group_name');
+    return false;
+}
 
 // return the files visibility level (institution, open, restricted);
 function get_visibility(){
